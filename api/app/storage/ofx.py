5362f4a3a3ff28d36a15985fa405e9e71e6c0f64
Chris Larabee <chris.larabee9@gmail.com>
2023-11-11 12:07:41 -0600

    Implemented OFX file reading functionality.

---
+++ api/app/storage/ofx.py
@@ -0,0 +1,87 @@
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+import re
+from typing import Iterable
+
+
+class UnexpectedOFXFormat(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(f"Unexpected OFX Format: {msg}")
+
+
+@dataclass
+class OFXTransaction:
+    fit_id: str
+    dt_posted: datetime
+    trn_amt: float
+    trn_type: str
+    name: str
+    memo: str
+
+
+@dataclass
+class OFXFileData:
+    bank_id: str
+    acct_id: str
+    acct_type: str
+    transactions: list[OFXTransaction]
+
+
+@dataclass
+class OFXTokenMatch:
+    token: str
+    found: str = ""
+
+
+def match_ofx_tokens(raw: str, tokens: Iterable[str]) -> dict[str, str]:
+    matches = {token: "" for token in tokens}
+
+    for token in matches:
+        m = re.search(rf"\<{token}\>(.*?)\<", raw)
+
+        if m:
+            matches[token] = m.groups()[0]
+        else:
+            raise UnexpectedOFXFormat(f"Could not find <{token}> token in {raw}.")
+
+    return matches
+
+
+def read_ofx_transaction_row(trn: str) -> OFXTransaction:
+    matches = match_ofx_tokens(
+        trn,
+        ["FITID", "DTPOSTED", "TRNAMT", "TRNTYPE", "NAME", "MEMO"],
+    )
+
+    # All found values should be populated at this point if the transaction row
+    # was in the expected format. Unexpected values should raise appropraite
+    # ValueErrors:
+    return OFXTransaction(
+        fit_id=matches["FITID"],
+        dt_posted=datetime.strptime(matches["DTPOSTED"], "%Y%m%d%H%M%S"),
+        trn_amt=float(matches["TRNAMT"]),
+        trn_type=matches["TRNTYPE"],
+        name=matches["NAME"],
+        memo=matches["MEMO"],
+    )
+
+
+def read_ofx_file(filepath: str | Path) -> OFXFileData:
+    raw_header: str = ""
+    raw_transactions = list[str]()
+    with open(filepath) as file:
+        for line in file:
+            if "<STMTTRN>" in line:
+                raw_transactions.append(line.strip())
+            else:
+                raw_header += line.strip()
+
+    header_matches = match_ofx_tokens(raw_header, ["BANKID", "ACCTID", "ACCTTYPE"])
+
+    return OFXFileData(
+        bank_id=header_matches["BANKID"],
+        acct_id=header_matches["ACCTID"],
+        acct_type=header_matches["ACCTTYPE"],
+        transactions=[read_ofx_transaction_row(trn) for trn in raw_transactions],
+    )
---
+++ api/tests/test_ofx.py
@@ -0,0 +1,112 @@
+from datetime import datetime
+
+import pytest
+from app.storage.ofx import (
+    OFXFileData,
+    OFXTransaction,
+    UnexpectedOFXFormat,
+    match_ofx_tokens,
+    read_ofx_file,
+    read_ofx_transaction_row,
+)
+
+
+def test_match_ofx_tokens():
+    expected = {
+        "FITID": "789_1011-S0200|123456",
+        "DTPOSTED": "20230828123115",
+        "TRNAMT": "-1.00",
+        "TRNTYPE": "DEBIT",
+        "NAME": "Foo",
+        "MEMO": "DebitCard, Withdrawal, Processed",
+    }
+    assert (
+        match_ofx_tokens(
+            "<STMTTRN><TRNTYPE>DEBIT<DTPOSTED>20230828123115<TRNAMT>-1.00"
+            "<FITID>789_1011-S0200|123456<NAME>Foo"
+            "<MEMO>DebitCard, Withdrawal, Processed</STMTTRN>",
+            ["FITID", "DTPOSTED", "TRNAMT", "TRNTYPE", "NAME", "MEMO"],
+        )
+        == expected
+    )
+
+    with pytest.raises(
+        UnexpectedOFXFormat, match="Unexpected OFX Format: Could not find <TRNAMT>"
+    ):
+        read_ofx_transaction_row(
+            "<STMTTRN><TRNTYPE>DEBIT<DTPOSTED>20230828123115<AMT>-1.00"
+            "<FITID>789_1011-S0200|123456<NAME>Foo"
+            "<MEMO>DebitCard, Withdrawal, Processed</STMTTRN>"
+        )
+
+
+def test_read_ofx_transaction_row():
+    assert read_ofx_transaction_row(
+        "<STMTTRN><TRNTYPE>DEBIT<DTPOSTED>20230828123115<TRNAMT>-1.00"
+        "<FITID>789_1011-S0200|123456<NAME>Foo"
+        "<MEMO>DebitCard, Withdrawal, Processed</STMTTRN>",
+    ) == OFXTransaction(
+        fit_id="789_1011-S0200|123456",
+        dt_posted=datetime(2023, 8, 28, 12, 31, 15),
+        trn_amt=-1.00,
+        trn_type="DEBIT",
+        name="Foo",
+        memo="DebitCard, Withdrawal, Processed",
+    )
+
+    with pytest.raises(ValueError, match="could not convert string to float: '1.oo'"):
+        read_ofx_transaction_row(
+            "<STMTTRN><TRNTYPE>DEBIT<DTPOSTED>20230828123115<TRNAMT>1.oo"
+            "<FITID>789_1011-S0200|123456<NAME>Foo"
+            "<MEMO>DebitCard, Withdrawal, Processed</STMTTRN>"
+        )
+
+
+def test_read_ofx_file():
+    assert read_ofx_file("tests/samples/acct_trns.ofx") == OFXFileData(
+        bank_id="123456",
+        acct_id="123_456-S0200",
+        acct_type="CHECKING",
+        transactions=[
+            OFXTransaction(
+                fit_id="789_1011-S0200|123456",
+                dt_posted=datetime(2023, 8, 28, 12, 31, 15),
+                trn_amt=-1.00,
+                trn_type="DEBIT",
+                name="Foo",
+                memo="DebitCard, Withdrawal, Processed",
+            ),
+            OFXTransaction(
+                fit_id="789_1011-S0200|123457",
+                dt_posted=datetime(2023, 8, 28),
+                trn_amt=-37.36,
+                trn_type="DEBIT",
+                name="Bar",
+                memo="DebitCard, Withdrawal, Processed",
+            ),
+            OFXTransaction(
+                fit_id="789_1011-S0200|123458",
+                dt_posted=datetime(2023, 8, 29),
+                trn_amt=-3.00,
+                trn_type="DEBIT",
+                name="Spam",
+                memo="DebitCard, Withdrawal, Processed",
+            ),
+            OFXTransaction(
+                fit_id="789_1011-S0200|123459",
+                dt_posted=datetime(2023, 8, 30),
+                trn_amt=-18.33,
+                trn_type="PAYMENT",
+                name="Eggs",
+                memo="BillPayment, Withdrawal, Processed",
+            ),
+            OFXTransaction(
+                fit_id="789_1011-S0200|123460",
+                dt_posted=datetime(2023, 8, 31),
+                trn_amt=208.58,
+                trn_type="DIRECTDEP",
+                name="Python",
+                memo="ACH, Deposit, Processed",
+            ),
+        ],
+    )
