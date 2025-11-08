142f7b7eafc48470568bcf72fef8e03663fd71bf
Chris Larabee <>
2025-11-08 11:21:46 -0600

    Updated backupdb script to pull only data and exclude alembic_version table.

--- api/scripts/backupdb.sh
+++ api/scripts/backupdb.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-docker exec -it mentha-app-db-1 pg_dump -U postgres -d mentha-db -f /exports/db-backup.sql
+docker exec -it mentha-app-db-1 pg_dump -U postgres \
+    -d mentha-db -T *alembic_version* --data-only \
+    -f /exports/db-backup.sql
